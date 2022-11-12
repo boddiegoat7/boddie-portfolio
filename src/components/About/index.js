2f39ff732a385625f76d5e9c6aa1ecb7c1da92ad
victor boddie <victorboddie21@gmail.com>
2022-11-12 09:45:18 -0500

    final commit added deployment

---
+++ src/components/About/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+  return (
+    <section>
+      <h1 id="about" className="center">
+        Victor Boddie
+      </h1>
+      <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      />
+      <p> Hello my name is Victor boddie.</p>
+    </section>
+  );
+}
+
+export default About;
